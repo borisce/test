96461189585c73fc2090c0e7956cd88ec67773dc
borisce <borcer99@gmail.com>
2019-12-14 17:18:06 +0100

    n fukcia

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -14,3 +14,17 @@ typedef struct nehnutelnost{
   char popis [203];
   struct nehnutelnost *dalsi;	
 } NEHNUTELNOST;
+
+NEHNUTELNOST * n_funkcia(FILE *fr, NEHNUTELNOST *prvy){
+  NEHNUTELNOST *akt, *pom2;
+  int pocet=0;
+  char znak[10], *pom;
+  if(prvy!=NULL){
+    akt=prvy;
+	while(akt!=NULL){  //uvolnenie pamate ak zoznam existoval
+	  pom2=akt;
+      akt=akt->dalsi;
+      free(pom2);	
+	}
+  	
+  } 
