13495aa3fdbc6b0c8883e162125302488f3b850e
borisce <borcer99@gmail.com>
2019-12-14 17:22:00 +0100

    dokonanie n

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -28,3 +28,40 @@ NEHNUTELNOST * n_funkcia(FILE *fr, NEHNUTELNOST *prvy){
 	}
   	
   } 
+  if((fr = fopen("reality.txt", "r")) == NULL)
+    printf("Zaznamy neboli nacitane\n");
+  else{
+    fgets(znak, 10, fr);
+	while(znak[0]=='&'){ 
+      	pocet+=1;
+        if(pocet==1){  //nacitanie 1. prvku zoznamu
+		  prvy = (NEHNUTELNOST *) malloc(sizeof(NEHNUTELNOST));
+		  fgets(prvy->typ_neh, 53, fr);
+		  fgets(prvy->miesto, 53, fr);
+		  fgets(prvy->ulica, 103, fr);
+		  fscanf(fr, "%d ", &(prvy->rozloha));
+		  fscanf(fr, "%d ", &(prvy->cena));
+		  fgets(prvy->popis, 203, fr);
+		  akt=prvy;
+		  akt->dalsi=NULL;
+	   }
+	   else{  //nacitanie ostatnych prvkov zoznamu
+	      akt->dalsi = (NEHNUTELNOST *) malloc(sizeof(NEHNUTELNOST));
+		  akt=akt->dalsi;
+		  fgets(akt->typ_neh, 50, fr);
+		  fgets(akt->miesto, 50, fr);
+		  fgets(akt->ulica, 100, fr);
+     	  fscanf(fr, "%d ", &(akt->rozloha));
+		  fscanf(fr, "%d ", &(akt->cena));
+		  fgets(akt->popis, 200, fr);
+		  akt->dalsi=NULL;	
+	   }
+	   pom=fgets(znak, 10, fr);
+	   if(pom==NULL)
+	    break;
+
+   }
+  }
+  printf("Nacitalo sa %d zaznamov\n", pocet);
+  return prvy;
+}
